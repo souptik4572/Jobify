a2785ffa8f217fe4a1e6b00743f8111fc364a66e
souptik4572 <souptiksarkar4572@gmail.com>
2021-05-14 20:19:08 +0530

    separated controllers for jobs

---
+++ controllers/jobs.js
@@ -0,0 +1,99 @@
+const Job = require('../models/job');
+
+// Get new job form
+const getNewJobForm = (req, res) => {
+	res.render('NewJob');
+};
+
+// Get edit form for existing job
+const getEditJobForm = (req, res) => {
+	const { id } = req.params;
+	Job.findById(id, (error, job) => {
+		if (error) {
+			console.log('Oops got an error while getting a particular job for edit');
+			return;
+		}
+		res.render('EditJob', { job });
+	});
+};
+
+// Get details of a particular job
+const getParticularJob = (req, res) => {
+	const { id } = req.params;
+	Job.findById(id, (error, job) => {
+		if (error) {
+			console.log('Oops got an error while getting a particular job');
+			return;
+		}
+		res.send(job);
+	});
+};
+
+// Submit edit form to update job details
+const editParticularJob = (req, res) => {
+	let { job } = req.body;
+	job.isJobOpen = !!job.isJobOpen;
+	const { id } = req.params;
+	Job.findByIdAndUpdate(id, job, (error, job) => {
+		if (error) {
+			console.log('Oops got an error while editing job', error);
+			return;
+		}
+		res.redirect('/jobs');
+	});
+};
+
+// Delete a particular job
+const deleteParticularJob = (req, res) => {
+	const { id } = req.params;
+	Job.findByIdAndDelete(id, (error, job) => {
+		if (error) {
+			console.log('Oops got an error while deleting a job', error);
+			return;
+		}
+		res.redirect('/jobs');
+	});
+};
+
+// Get list of all jobs
+const getAllJobs = (req, res) => {
+	const employer = {
+		id: req.user._id,
+		username: req.user.username,
+	};
+	Job.find({ employer }, (error, jobs) => {
+		if (error) {
+			console.log('Oops got an error while fetching all jobs', error);
+			return;
+		}
+		res.render('EmployerDashboard', { jobs });
+	});
+};
+
+// Create a new job
+const createNewJob = (req, res) => {
+	const { job } = req.body;
+	const { _id, username } = req.user;
+	job.employer = {
+		id: _id,
+		username: username,
+	};
+	job.date = new Date();
+	Job.create(job, (error, job) => {
+		if (error) {
+			console.log('Oops got an error while creating new job', error);
+			return;
+		}
+		res.redirect('/jobs');
+	});
+};
+
+module.exports = {
+	getNewJobForm,
+	getEditJobForm,
+	getParticularJob,
+	editParticularJob,
+	deleteParticularJob,
+	getAllJobs,
+	createNewJob,
+};
--- routes/job.js
+++ routes/job.js
@@ -2,88 +2,28 @@ const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../middleware');
 
-const User = require('../models/user');
-const Job = require('../models/job');
+const {
+	getNewJobForm,
+	getEditJobForm,
+	getParticularJob,
+	editParticularJob,
+	deleteParticularJob,
+	getAllJobs,
+	createNewJob,
+} = require('../controllers/jobs');
 
-router.get('/new', isLoggedIn, (req, res) => {
-	res.render('NewJob');
-});
+router.get('/new', isLoggedIn, getNewJobForm);
 
-router.get('/:id/edit', isLoggedIn, (req, res) => {
-	const { id } = req.params;
-	Job.findById(id, (error, job) => {
-		if (error) {
-			console.log('Oops got an error while getting a particular job for edit');
-			return;
-		}
-		res.render('EditJob', { job });
-	});
-});
+router.get('/:id/edit', isLoggedIn, getEditJobForm);
 
-router.get('/:id', isLoggedIn, (req, res) => {
-	const { id } = req.params;
-	Job.findById(id, (error, job) => {
-		if (error) {
-			console.log('Oops got an error while getting a particular job');
-			return;
-		}
-		res.send(job);
-	});
-});
+router.get('/:id', isLoggedIn, getParticularJob);
 
-router.put('/:id', (req, res) => {
-	let { job } = req.body;
-	job.isJobOpen = !!job.isJobOpen;
-	const { id } = req.params;
-	Job.findByIdAndUpdate(id, job, (error, job) => {
-		if (error) {
-			console.log('Oops got an error while editing job', error);
-			return;
-		}
-		res.redirect('/jobs');
-	});
-});
+router.put('/:id', editParticularJob);
 
-router.delete('/:id', (req, res) => {
-	const { id } = req.params;
-	Job.findByIdAndDelete(id, (error, job) => {
-		if (error) {
-			console.log('Oops got an error while deleting a job', error);
-			return;
-		}
-		res.redirect('/jobs');
-	});
-});
+router.delete('/:id', deleteParticularJob);
 
-router.get('/', isLoggedIn, (req, res) => {
-	const employer = {
-		id: req.user._id,
-		username: req.user.username,
-	};
-	Job.find({ employer }, (error, jobs) => {
-		if (error) {
-			console.log('Oops got an error while fetching all jobs', error);
-			return;
-		}
-		res.render('EmployerDashboard', { jobs });
-	});
-});
+router.get('/', isLoggedIn, getAllJobs);
 
-router.post('/', (req, res) => {
-	const { job } = req.body;
-	const { _id, username } = req.user;
-	job.employer = {
-		id: _id,
-		username: username,
-	};
-	job.date = new Date();
-	Job.create(job, (error, job) => {
-		if (error) {
-			console.log('Oops got an error while creating new job', error);
-			return;
-		}
-		res.redirect('/jobs');
-	});
-});
+router.post('/', createNewJob);
 
 module.exports = router;
